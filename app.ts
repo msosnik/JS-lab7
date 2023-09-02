419e4cbaa3e4969eeacbd3a22af4fcea197b3dc9
ssosnik <ssosnik@gmail.com>
2023-09-02 20:20:47 +0200

    Add app.ts and fix bug

---
+++ app.ts
@@ -0,0 +1,20 @@
+// @deno-types="npm:@types/express@^4"
+import express, { Express, Request, Response } from "npm:express@^4";
+import morgan from "npm:morgan@^1";
+import "npm:pug@^3";
+const app: Express = express();
+const deno_logo = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Deno_2021.svg/120px-Deno_2021.svg.png';
+app.set('views', './views');
+app.set('view engine', 'pug');
+app.locals.pretty = app.get('env') === 'development';
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.get('/', function (_req: Request, res: Request) {
+    res.render('index', {deno_logo});
+});
+app.post('/', function (req: Response, res: Response) {
+    res.send(`Hello '${req.body.name}'`); 
+});
+app.listen(8000, function () {
+    console.log('The application is available on port 8000');
+});    
