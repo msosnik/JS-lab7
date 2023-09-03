22a9a93fbc7e5ea04d2d767dfb7fa6051c9f58c1
ssosnik <ssosnik@gmail.com>
2023-09-03 16:24:36 +0200

    change from expres to oak Add ejs html-template Add dejs-engine add router and correct routing

--- lab6-app1-oak.ts
+++ lab6-app1-oak.ts
@@ -1,8 +1,36 @@
-import { Application, Context } from 'https://deno.land/x/oak/mod.ts';
+import { Application, Context, Router } from 'https://deno.land/x/oak/mod.ts';
+import { dejsEngine, oakAdapter, viewEngine } from "https://deno.land/x/view_engine@v10.6.0/mod.ts";
+
+function encodeXML(text: string): string {
+return text.replace(/[<>&'"]/g, function (char) {
+    switch (char) {
+    case '<': return '&lt;';
+    case '>': return '&gt;';
+    case '&': return '&amp;';
+    case "'": return '&apos;';
+    case '"': return '&quot;';
+    default: return char;
+    }
+  });
+}
 
 const app = new Application();
+const router = new Router();
 
-app.use(async (ctx: Context) => {
+// Initialize Oak View Engine
+app.use(
+    viewEngine(oakAdapter, dejsEngine, {
+      viewRoot: "./views-lab6-ejs",
+    })
+  );
+
+// Route for the homepage
+router.get('/', async (ctx: Context) => {
+    // Handle GET request for the homepage
+    await ctx.render('index'); 
+  });
+
+router.all('/submit', async (ctx: Context) => {
   let name = '';
 
   if (ctx.request.method === 'GET') {
@@ -17,24 +45,27 @@ app.use(async (ctx: Context) => {
   console.log('\x1B[35mctx.request.url\x1B[0m =', ctx.request.url);
   console.log('\x1B[35mctx.request.body\x1B[0m  =', name);
 
-  const accepts = ctx.request.accepts(['html', 'text', 'json', 'xml']);
-  switch (accepts) {
-    case 'json':
+  const acceptHeader = ctx.request.headers.get('Accept');
+  
+  if (acceptHeader) {
+    if (acceptHeader.includes('application/json')) {
       ctx.response.headers.set('Content-Type', 'application/json');
       ctx.response.body = JSON.stringify({ welcome: `Hello '${name}'` });
       console.log(`The server sent a \x1B[31mJSON\x1B[0m document to the browser`);
-      break;
-    case 'xml':
+    } else if (acceptHeader.includes('application/xml')) {
+      name = name !== undefined ? encodeXML(name) : '';
       ctx.response.headers.set('Content-Type', 'application/xml');
       ctx.response.body = `<welcome>Hello '${name}'</welcome>`;
       console.log(`The server sent an \x1B[31mXML\x1B[0m document to the browser`);
-      break;
-    default:
+    } else {
       ctx.response.headers.set('Content-Type', 'text/plain');
       ctx.response.body = `Hello '${name}'`;
       console.log(`The server sent a \x1B[31mplain text\x1B[0m document to the browser`);
+    }
   }
 });
+
+app.use(router.routes());
 
 console.log('The server was started on port 8000');
 console.log('To stop the server, press "CTRL + C"');
